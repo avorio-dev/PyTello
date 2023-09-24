0084afa4f1eb634eca839815d20cea394f8628cc
avorio-dev <avorio.dev@gmail.com>
2023-09-24 09:24:20 +0200

    Update submodules

--- submodulesUpdater.py
+++ submodulesUpdater.py
@@ -1,9 +1,10 @@
 import submodules.PyUtils.Misc.SubmodulesUpdater as SubmodUpd
 
 
-def update_submodules():
-    SubmodUpd.update_submodules()
+def update_submodules(project_name):
+    SubmodUpd.update_submodules(project_name)
 
 
 if __name__ == "__main__":
-    update_submodules()
+    project_name = "PyTello"
+    update_submodules(project_name)
